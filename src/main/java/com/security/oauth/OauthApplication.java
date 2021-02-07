cece0055b983eaec5021e748e54bde7854338645
woohyuk <dngur6344@gmail.com>
2021-02-07 21:35:42 +0900

    Initial commit

---
+++ src/main/java/com/security/oauth/OauthApplication.java
@@ -0,0 +1,15 @@
+package com.security.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//@EnableEurekaClient
+@SpringBootApplication
+public class OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApplication.class, args);
+	}
+
+}
