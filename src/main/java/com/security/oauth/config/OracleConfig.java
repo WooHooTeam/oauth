b6ad10b4a62b28a6e3c6dcf326f703a1b2028acb
woohyuk <dngur6344@gmail.com>
2021-07-11 18:53:13 +0900

    Oracle DB Wallet 설정 추가

---
+++ src/main/java/com/security/oauth/config/OracleConfig.java
@@ -0,0 +1,33 @@
+package com.security.oauth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.io.File;
+
+@Configuration
+public class OracleConfig {
+
+    @Value("${oracle_wallet_path}")
+    String url;
+
+    @Value("${oracle_wallet_id}")
+    String username;
+
+    @Value("${oracle_wallet_password}")
+    String password;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(oracle.jdbc.driver.OracleDriver.class.getName());
+        String path = "/var/oracle/wallet";
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        return ds;
+    }
+}
